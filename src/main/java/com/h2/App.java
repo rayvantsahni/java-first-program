5e2e9837aca099fac755ef04e8dfbf09f0322bf4
Rayvant Sahni <500061722@stu.upes.ac.in>
2021-04-09 18:41:03 +0530

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -6,12 +6,20 @@ package com.h2;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
+    }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for (int number: numbers)
+            sum += number;
+
+        return sum;
     }
 }
---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,39 @@
+package com.h2;
+
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates
+{
+    public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+
+    public static float getRates(int loanTermInYears) {
+        if (bestRates.containsKey(loanTermInYears))
+            return bestRates.get(loanTermInYears);
+        return .0f;
+    }
+
+    private static boolean areEqual(float first, float second) {
+        return Math.abs(first - second) < Float.MIN_NORMAL;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println(MessageFormat.format("Hello {0}", name));
+
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+
+        float bestRate = getRates(loanTermInYears);
+        if (areEqual(bestRate, .0f))
+            System.out.println(MessageFormat.format("No available rates for term: {0} years", loanTermInYears));
+        else
+            System.out.println(MessageFormat.format("Best Available Rate: {0} %", bestRate));
+
+        scanner.close();
+    }
+}
